138b1d21d2e2c6463e0c7da3e73fe0e7f454c834
Ahmed Rayhan Primadedas <rayhanprima99@gmail.com>
2021-11-14 00:55:13 +0700

    Update main.js

--- src/main.js
+++ src/main.js
@@ -4,14 +4,6 @@ import { stdin as input, stdout as output } from 'process';
 
 const main = () => {
   try {
-    // const prompt = readline.createInterface({ input, output })
-
-    // function ask(question) {
-    //   return new Promise((resolve, reject) => {
-    //     prompt.question(question, (input) => resolve(input) );
-    //   });
-    // }
-
     let command = core.getInput('string');
     let separator = 'hehehe';
     let limit = parseInt(core.getInput('limit'), 10);
@@ -26,41 +18,11 @@ const main = () => {
       core.setOutput(`_${index}`, part);
     });
 
-    core.setOutput('length', parts.length);
+    core.setOutput('length', 'testing ' + parts.length);
     core.setOutput('separator', separator);
   } catch (error) {
     core.setFailed(error.message);
   }
-
-  //   ask('Your command? ')
-  //     .then(commandResult => {
-  //       command = commandResult
-  //       return ask('Your separator? ')
-  //     })
-  //     .then(separatorResult => {
-  //       separator = separatorResult;
-  //       return ask('Your limit? ')
-  //     })
-  //     .then(limitResult => {
-  //       limit = limitResult;
-  //     })
-  //     .then(() => {
-  //       const parts = command.split(separator, limit);
-
-  //       parts.forEach((part, index) => {
-  //         core.setOutput(`_${index}`, part);
-  //       });
-      
-  //       core.setOutput('length', parts.length);
-
-  //       console.log({ command, separator, limit });
-  //     })
-  //     .then(() => {
-  //       prompt.close()
-  //     });
-  // } catch (error) {
-  //   core.setFailed(error.message);
-  // }
 };
 
 main();
