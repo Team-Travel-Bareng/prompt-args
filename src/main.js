2c0b44bd7b2c5543b15d63c08f0236410979021f
qassandrach <qassandra.chaidir@tokopedia.com>
2021-11-13 23:24:35 +0700

    add input from workflows

--- src/main.js
+++ src/main.js
@@ -4,47 +4,62 @@ import { stdin as input, stdout as output } from 'process';
 
 const main = () => {
   try {
-    const prompt = readline.createInterface({ input, output })
+    // const prompt = readline.createInterface({ input, output })
 
-    function ask(question) {
-      return new Promise((resolve, reject) => {
-        prompt.question(question, (input) => resolve(input) );
-      });
-    }
+    // function ask(question) {
+    //   return new Promise((resolve, reject) => {
+    //     prompt.question(question, (input) => resolve(input) );
+    //   });
+    // }
 
-    let command = '';
-    let separator = '.';
-    let limit = -1;
+    let command = core.getInput('string');
+    let separator = core.getInput('separator');
+    let limit = parseInt(core.getInput('limit'), 10);
 
-    ask('Your command? ')
-      .then(commandResult => {
-        command = commandResult
-        return ask('Your separator? ')
-      })
-      .then(separatorResult => {
-        separator = separatorResult;
-        return ask('Your limit? ')
-      })
-      .then(limitResult => {
-        limit = limitResult;
-      })
-      .then(() => {
-        const parts = command.split(separator, limit);
+    console.log(`Input string : ${command}`);
+    console.log(`Separator    : ${separator}`);
+    console.log(`Limit        : ${limit}`);
 
-        parts.forEach((part, index) => {
-          core.setOutput(`_${index}`, part);
-        });
-      
-        core.setOutput('length', parts.length);
+    let parts = command.split(separator, limit);
 
-        console.log({ command, separator, limit });
-      })
-      .then(() => {
-        prompt.close()
-      });
+    parts.forEach((part, index) => {
+      core.setOutput(`_${index}`, part);
+    });
+
+    core.setOutput('length', parts.length);
   } catch (error) {
     core.setFailed(error.message);
   }
+
+  //   ask('Your command? ')
+  //     .then(commandResult => {
+  //       command = commandResult
+  //       return ask('Your separator? ')
+  //     })
+  //     .then(separatorResult => {
+  //       separator = separatorResult;
+  //       return ask('Your limit? ')
+  //     })
+  //     .then(limitResult => {
+  //       limit = limitResult;
+  //     })
+  //     .then(() => {
+  //       const parts = command.split(separator, limit);
+
+  //       parts.forEach((part, index) => {
+  //         core.setOutput(`_${index}`, part);
+  //       });
+      
+  //       core.setOutput('length', parts.length);
+
+  //       console.log({ command, separator, limit });
+  //     })
+  //     .then(() => {
+  //       prompt.close()
+  //     });
+  // } catch (error) {
+  //   core.setFailed(error.message);
+  // }
 };
 
 main();
